2421ea1a8350bf13f51a04b0009d7c44784a4e18
RHenriques9 <107552006+RHenriques9@users.noreply.github.com>
2022-06-18 19:09:59 +0100

    Add files via upload

---
+++ Loginpage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" href="LoginPage.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="main" class="clearfix">
+      <div id="coll">
+        <img id="profile_picture" src="Resources/user.png" />
+        <h1 onclick="window.location.href='Login.php'">Login</h1>
+        <h2>Sign up?</h2>
+      </div>
+      <div id="colr">
+        <img id="logo" src="Resources/runner.png" />
+        <h3>Record your Workouts!</h3>
+        <h4>Manage all your Workouts and compete with friends!</h4>
+      </div>
+    </div>
+  </body>
+</html>
